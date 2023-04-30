104c27b1be8dedca9ccc960630dd87ca26645471
PanicP <filmous2136@gmail.com>
2023-04-30 23:43:25 +0200

    a

---
+++ lab3/AverageReducer.java
@@ -0,0 +1,37 @@
+package stubs;
+
+import java.io.IOException;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AverageReducer
+  extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+
+  //  @Override
+  //  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+  //      throws IOException, InterruptedException {
+  //
+  //    /*
+  //     * TODO implement
+  //     */
+  //
+  //  }
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+    throws IOException, InterruptedException {
+    long wordLengthSum = 0;
+    long wordCount = 0;
+
+    for (IntWritable value : values) {
+      wordLengthSum += value.get();
+      wordCount++;
+    }
+    if (wordCount != 0) {
+      double result = (double) wordLengthSum / (double) wordCount;
+      context.write(key, new DoubleWritable(result));
+    }
+  }
+}
